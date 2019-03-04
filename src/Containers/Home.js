38038697c526b06031c6167fca1fd281dd2c0d51
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-03-04 10:34:27 -0500

    some more home styling

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -14,19 +14,27 @@ class Home extends Component {
   render() {
     return (
       <div>
-        <Popup trigger={
-            <div className="ui button" onClick> Login </div>} modal
-            position="right center">
-            <div>{<LoginForm changeHandler={this.props.changeHandler} loginHandler={this.props.loginHandler}/>}</div>
-        </Popup>
+        <div className="nav-buttons">
+          <Popup trigger={
+              <div className="ui button" onClick> Login </div>} modal
+              position="right center">
+              <div>{<LoginForm changeHandler={this.props.changeHandler} loginHandler={this.props.loginHandler}/>}</div>
+          </Popup>
 
-        <Popup trigger={
-            <div className="ui button" onClick> Sign Up </div>} modal
-            position="right center">
-            <div>{<SignUpForm changeHandler={this.props.changeHandler} signupHandler={this.props.signupHandler}/>}</div>
-        </Popup>
+          <Popup trigger={
+              <div className="ui button" onClick> Sign Up </div>} modal
+              position="right center">
+              <div>{<SignUpForm changeHandler={this.props.changeHandler} signupHandler={this.props.signupHandler}/>}</div>
+          </Popup>
+        </div>
+        
           <h1 className="main-title">SaveSpace<span id="nyc">nyc</span></h1>
-          <SearchForm className="search" changeHandler={this.props.changeHandler} term={this.props.term} location={this.props.location} submitHandler={this.props.submitHandler}/>
+          <div className="search">
+            <SearchForm className="search" changeHandler={this.props.changeHandler} term={this.props.term} location={this.props.location} submitHandler={this.props.submitHandler}/>
+          </div>
+          <div>
+            <h3 className="home-prompt">When you want to: <span className="home-prompt-slide">introduce your girlfriend that you met on HER to your parents</span></h3>
+          </div>
       </div>
     );
   }
