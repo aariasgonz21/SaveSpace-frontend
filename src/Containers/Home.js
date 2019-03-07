599613b291078d35574694ab7e7d011225e8587d
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-03-07 11:20:02 -0500

    about us

--- src/Containers/Home.js
+++ src/Containers/Home.js
@@ -47,13 +47,28 @@ class Home extends Component {
           {this.loginToggle()}
           <img className="home-background" src='../savespace-hands2.svg' alt="background-img"/>
           <h1 className="main-title">SaveSpace<span id="nyc">nyc</span></h1>
+
           <div className="search">
             <SearchForm className="search" changeHandler={this.props.changeHandler} term={this.props.term} location={this.props.location} submitHandler={this.props.submitHandler}/>
           </div>
+
           <div>
             <h3 className="home-prompt">When you want to: <span className="home-prompt-slide">introduce your girlfriend that you met on HER to your parents</span></h3>
             <h1 className="about-prompt">Wanna know our backstory?</h1>
             <img id="down-arrow" src="../download.svg" alt="down arrow"/>
+          </div>
+
+          <div className="about-us">
+            <h2 className="about-title">This is the story of 4 women of color and their experience bar hopping in College Park, Maryland.</h2>
+            <div className="about-p">
+              <h3 className="about-p1">Ana, Chanel and Wuraola went to their first Technica and decided to go around the town and grab a beer. But each bar they passed by had a more unwelcoming vibe than the next. When they sat down the next day to decide on a project,</h3>
+
+              <h3 className="about-p2"> Wura said, "What should we do our project on?".</h3>
+
+              <h3 className="about-p2">Chanel said, "We should make a project based on how we felt yesterday!".</h3>
+
+              <h3 className="about-p3">Ana said, "Aha! Let's call it <span>SaveSpace</span>"</h3>
+            </div>
           </div>
       </div>
     );
