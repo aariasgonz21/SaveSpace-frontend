12cf4b2406b6b1e66c469da84c9076d68d9b7107
Ana Arias-Gonzalez <ana.ariasgonz@gmail.com>
2019-03-26 18:17:04 -0400

    reviews jic

--- src/Components/Review.js
+++ src/Components/Review.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 
 class Review extends Component {
-  estName = () => {
+
+reviewHeader = () => {
     if(this.props.isProfile === true){
       return <Link className="est-link" to={`/establishments/${this.props.review.yelp_id}`}>{this.props.review.establishment_name}</Link>
     }
     else{
-      return ""
+      return <h1 className="username">{this.props.review.user.first_name}</h1>
      }
   }
 
   render() {
-    return(
+    return (
       <div className="review-container">
-        <h1 className="username">{this.props.review.user.first_name}</h1>
-        <div>{this.estName()}</div>
+        <div>{this.reviewHeader()}</div>
         <h2 className="review-title">"{this.props.review.name}"</h2>
         <div className="search-rating-bar rev-rate">
           <h3>Women Rating: {this.props.review.women_rating === 0 ? 'n/a' : this.props.review.women_rating}</h3>
@@ -24,7 +24,7 @@ class Review extends Component {
         </div>
         <h4 className="review-text">{this.props.review.review_text}</h4>
       </div>
-    )
+    );
   }
 }
 
--- src/Containers/ReviewContainer.js
+++ src/Containers/ReviewContainer.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import Review from '../Components/Review';
 
 const ReviewContainer = props => {
-    console.log(props.reviews);
+    //console.log(props.reviews);
     let singleReview = props.reviews.map(review => <Review key={review.id} review={review} user={props.user} isProfile={props.isProfile}/>)
     return (
       <div>
